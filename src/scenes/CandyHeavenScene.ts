251c6812db76c8338293e7b29c974dc71803d8b4
Brown, David (US - Baltimore) <davidbrown3@deloitte.com>
2019-06-02 16:26:22 -0400

    added more candy to candy heaven

--- src/scenes/CandyHeavenScene.ts
+++ src/scenes/CandyHeavenScene.ts
@@ -12,6 +12,13 @@ class CandyHeavenScene extends Phaser.Scene {
   lollipopWhiteGreen: Phaser.Physics.Arcade.Sprite;
   lollipopBase3: Phaser.Physics.Arcade.Sprite;
   lollipopWhiteRed: Phaser.Physics.Arcade.Sprite;
+  gummyWormRedEnd: Phaser.Physics.Arcade.Sprite;
+  gummyWormRedHead: Phaser.Physics.Arcade.Sprite;
+  gummyWormRedMid: Phaser.Physics.Arcade.Sprite;
+  candyGreen: Phaser.Physics.Arcade.Sprite;
+  wafflePink: Phaser.Physics.Arcade.Sprite;
+  cherry: Phaser.Physics.Arcade.Sprite;
+  heart: Phaser.Physics.Arcade.Sprite;
   camera: Phaser.Cameras.Scene2D.Camera;
 
   fadeOutStarted: boolean;
@@ -43,6 +50,13 @@ class CandyHeavenScene extends Phaser.Scene {
   preload() {
     //visuals
     this.load.image('gameOver', 'assets/sprites/gameOver.png');
+    this.load.image('wormEnd', 'assets/sprites/gummyWormRedEnd.png');
+    this.load.image('wormHead', 'assets/sprites/gummyWormRedHead.png');
+    this.load.image('wormMid', 'assets/sprites/gummyWormRedMid.png');
+    this.load.image('greenCandy', 'assets/sprites/candyGreen.png');
+    this.load.image('pinkWaffle', 'assets/sprites/wafflePink.png');
+    this.load.image('cherryImage', 'assets/sprites/cherry.png');
+    this.load.image('heartImage', 'assets/sprites/heart.png');
   }
 
   create() {
@@ -74,6 +88,24 @@ class CandyHeavenScene extends Phaser.Scene {
     this.lollipopBase3.setScale(.6).refreshBody();
     this.lollipopWhiteRed = this.physics.add.staticSprite(this.cameras.main.centerX + 230, this.cameras.main.centerY + 30, 'lolliWhiteRed');
     this.lollipopWhiteRed.setScale(.6).refreshBody();
+    this.gummyWormRedHead = this.physics.add.staticSprite(this.cameras.main.centerX + 140, this.cameras.main.centerY + 124, 'wormHead');
+    this.gummyWormRedHead.scaleX = -.6
+    this.gummyWormRedHead.scaleY = .6
+    this.gummyWormRedHead.refreshBody();
+    this.gummyWormRedMid = this.physics.add.staticSprite(this.cameras.main.centerX + 230, this.cameras.main.centerY + 124, 'wormMid');
+    this.gummyWormRedMid.setScale(.6).refreshBody();
+    this.gummyWormRedEnd = this.physics.add.staticSprite(this.cameras.main.centerX + 280, this.cameras.main.centerY + 124, 'wormEnd');
+    this.gummyWormRedEnd.setScale(.6).refreshBody();
+    this.candyGreen = this.physics.add.staticSprite(this.cameras.main.centerX + 60, this.cameras.main.centerY  + 124, 'greenCandy');
+    this.candyGreen.setScale(.6).refreshBody();
+    this.wafflePink = this.physics.add.staticSprite(this.cameras.main.centerX + 280, this.cameras.main.centerY, 'pinkWaffle');
+    this.wafflePink.setScale(.6).refreshBody();
+    this.heart = this.physics.add.staticSprite(this.cameras.main.centerX + 90, this.cameras.main.centerY + 100, 'heartImage');
+    this.heart.setScale(.6).refreshBody();
+    this.cherry = this.physics.add.staticSprite(this.cameras.main.centerX + 130, this.cameras.main.centerY + 70, 'cherryImage');
+    this.cherry.setScale(.6).refreshBody();
+
+
 
     console.log('navigator.userAgent', navigator.userAgent);
 
